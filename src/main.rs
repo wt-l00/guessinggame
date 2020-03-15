78bae36bb08757144cd53a2e7977cc4520d06333
wt-l00 <ei13suke@gmail.com>
2020-03-16 08:32:01 +0900

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+    println!("Please input your number");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed read line");
+    
+    println!("you guessed {}", guess);
 }
